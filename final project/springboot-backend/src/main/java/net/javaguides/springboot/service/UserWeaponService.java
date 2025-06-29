33bd4df8fc04c70a63dd46edd070cfd40bd52b73
Amishi24 <150577760+Amishi24@users.noreply.github.com>
2025-06-29 15:06:39 +0530

    Delete final project directory

--- final project/springboot-backend/src/main/java/net/javaguides/springboot/service/UserWeaponService.java
+++
@@ -1,8 +0,0 @@
-package net.javaguides.springboot.service;
-
-import java.util.List;
-import net.javaguides.springboot.entity.Weapon;
-
-public interface UserWeaponService {
-    List<Weapon> getMyWeapons(Integer userId);
-}
