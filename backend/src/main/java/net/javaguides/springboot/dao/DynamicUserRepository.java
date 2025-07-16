f93d9149fcbb596fdbbb20e9d5ac8a0e151fa1e4
Amishi24 <150577760+Amishi24@users.noreply.github.com>
2025-07-16 22:01:06 +0530

    Update DynamicUserRepository.java

--- backend/src/main/java/net/javaguides/springboot/dao/DynamicUserRepository.java
+++ backend/src/main/java/net/javaguides/springboot/dao/DynamicUserRepository.java
@@ -1,12 +1,17 @@
 package net.javaguides.springboot.dao;
 
 import net.javaguides.springboot.entity.DynamicUser;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.Optional;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-
 public interface DynamicUserRepository extends JpaRepository<DynamicUser, Integer> {
-	boolean existsByUserRole(String roleName);
-	Optional<DynamicUser> findByUserName(String userName);
+    boolean existsByUserRole(String roleName);
+    Optional<DynamicUser> findByUserName(String userName);
+    List<DynamicUser> findByUserRole(String userRole);
+    
+    @Query("SELECT DISTINCT u.userRole FROM DynamicUser u")
+    List<String> findDistinctUserRoles();
 }
